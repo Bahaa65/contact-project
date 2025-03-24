d2fb779bdb4332fcf52e486f75240b1e3a7db79c
Bahaa65 <bahaamohammed955@gmail.com>
2025-03-25 00:32:58 +0200

    feat: Refactor ContactList to use Chakra UI and add delete functionality

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
+import { Box, Text, VStack } from '@chakra-ui/react';
 import ContactCard from './ContactCard';
 
-type ContactListProps = {
-  contacts: { name: string; phone: string; email: string }[];
+type Contact = {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
 };
 
-const ContactList: React.FC<ContactListProps> = ({ contacts }) => {
+type ContactListProps = {
+  contacts: Contact[];
+  onDeleteContact: (id: string) => void;
+  onEditContact: (id: string) => void;
+};
+
+const ContactList: React.FC<ContactListProps> = ({ contacts, onDeleteContact }) => {
+  if (contacts.length === 0) {
+    return (
+      <Box textAlign="center" py={10}>
+        <Text fontSize="lg" color="gray.500">
+          No contacts found. Add some contacts to get started!
+        </Text>
+      </Box>
+    );
+  }
+
   return (
-    <div className="contact-list">
-      {contacts.map((contact, index) => (
+    <VStack spacing={4} align="stretch">
+      {contacts.map((contact) => (
         <ContactCard
-          key={index}
+          key={contact.id}
+          id={contact.id}
           name={contact.name}
           phone={contact.phone}
           email={contact.email}
+          onDelete={() => onDeleteContact(contact.id)}
         />
       ))}
-    </div>
+    </VStack>
   );
 };
 
