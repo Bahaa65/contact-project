9577aa2ac4b34671c9034209781c381da5ae740c
Bahaa65 <bahaamohammed955@gmail.com>
2025-03-25 00:30:55 +0200

    feat: Refactor Header to use Chakra UI components and add color mode toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Box, Heading, Container, Flex } from '@chakra-ui/react';
+import { ColorModeButton } from './ui/color-mode';
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <h1>My Contacts App</h1>
-    </header>
+    <Box bg="teal.500" color="white" py={4} shadow="md">
+      <Container maxW="container.md">
+        <Flex justify="space-between" align="center">
+          <Heading size="lg">Contact Manager</Heading>
+          <ColorModeButton />
+        </Flex>
+      </Container>
+    </Box>
   );
 };
 
